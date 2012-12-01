8da5200ed9b50ce2c8ca85418c162bc567bbc4b8
Niko <ed@bombenlabor.de>
2012-12-01 05:02:00 +0100

    alles neu

--- login/tinymce/jscripts/plugins/jbimages/ci/application/language/de/jbstrings_lang.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$lang['jb_blankpage_message'] = "Das Hochladen hat noch nicht begonnen, oder hat begonnen, ist aber noch nicht beendet, oder der Browser konnte den Ziel-Ordner nicht erreichen.";
-
-/* End of file jbstrings_lang.php */
--- login/tinymce/jscripts/plugins/jbimages/langs/de.js
+++
@@ -1,3 +0,0 @@
-tinyMCE.addI18n('de.jbimages',{
-	desc : 'Ein Bild hochladen'
-});
