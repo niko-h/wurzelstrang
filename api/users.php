7fa6c0eb20be6e9e7f8a66ad44c7f49876de17f5
lbenedix <lukas.benedix@fu-berlin.de>
2015-02-05 21:56:49 +0100

    refactored delete api for users

--- api/users.php
+++ api/users.php
@@ -22,23 +22,39 @@ $app->get( '/users', function () {
 } );
 
 
+// get user info
 $app->get( '/users/:id', function ( $user_id ) {
     checkAuthorization( Slim::getInstance()->request() );
     $query = 'SELECT user_email FROM users WHERE id = :user_id;';
 
     try {
-        $result = fetchFromDB( $query, [ 'user_id' => $user_id ] )[0];
+        $result = fetchFromDB( $query, [ 'user_id' => $user_id ] )[ 0 ];
 
         $siteadmin_query = 'select site_id from site_admins where user_id = :user_id';
-        $sites = fetchFromDB($siteadmin_query, ['user_id'=>$user_id]);
-        $result['sites'] = array();
-        foreach ($sites as &$row) {
-            array_push($result['sites'],intval($row['site_id']));
+        $sites = fetchFromDB( $siteadmin_query, [ 'user_id' => $user_id ] );
+        $result[ 'sites' ] = array();
+        foreach( $sites as &$row ) {
+            array_push( $result[ 'sites' ], intval( $row[ 'site_id' ] ) );
         }
 
         echo json_encode( $result );
     } catch( PDOException $e ) {
         echo '{"usererror":{"text":' . $e->getMessage() . '}}';
+    }
+} );
+
+
+// deleteUser
+$app->delete( '/users/:id', function ( $user_id ) {
+    $request = Slim::getInstance()->request();
+    checkAuthorization( $request );
+
+    $query = 'DELETE FROM users WHERE id = :user_id AND admin != 1;';
+    try {
+        updateDB( $query, [ 'user_id' => $user_id ] );
+        echo '{"deleted":' . $user_id . '}';
+    } catch( PDOException $e ) {
+        echo '{"deleteusererror":{"text":"Fehler beim L&ouml;schen."}}';
     }
 } );
 
@@ -73,17 +89,3 @@ $app->post( '/users', function () {
     }
 } );
 
-// deleteUser
-$app->delete( '/users', function () {
-    $request = Slim::getInstance()->request();
-    checkAuthorization( $request );
-    $user = json_decode( $request->getBody() );
-
-    $query = 'DELETE FROM users WHERE user_email = :user_email AND admin != 1;';
-    try {
-        updateDB( $query, [ 'user_email' => $user->email ] );
-        echo '{"deleted":' . $user->email . '}';
-    } catch( PDOException $e ) {
-        echo '{"deleteusererror":{"text":"Fehler beim L&ouml;schen."}}';
-    }
-} );
