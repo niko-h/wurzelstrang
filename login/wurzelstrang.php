2d32894864f7ebf2fed0f870a24d2c563b0ba322
Niko <cvs@nikolaushoefer.de>
2015-02-14 12:00:43 +0100

    Add DEBUG to you config ;) [FIX] - reinserted debug-handler.

--- login/wurzelstrang.php
+++ login/wurzelstrang.php
@@ -208,9 +208,9 @@ header( "Content-Type: text/html; charset=utf-8" );
 
 <script type="text/javascript"> 
     var ws_debug = false;
-//    if (<?php //echo DEBUG ?>//) {
-//        ws_debug = true;
-//    };
+    if (<?php echo DEBUG ?>) {
+        ws_debug = true;
+    };
     if (!ws_debug) { console.log = function() {}; }
 
     var apikey = <?php echo '"'.APIKEY.'"' ?>;
